bf4f8fa3bbe6cd1cf3c6236886c9507df7d91ec0
PhilipPartsch <95444300+PhilipPartsch@users.noreply.github.com>
2024-10-02 23:27:25 +0200

    added custom.css

--- sphinx-config/conf.py
+++ sphinx-config/conf.py
@@ -35,12 +35,12 @@ html_theme = 'sphinx_rtd_theme'
 #html_theme = 'alabaster'
 #html_theme = 'sphinx_immaterial'
 
-#html_css_files = ['custom.css']
+html_css_files = ['custom.css']
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
